b6675d774ec7059f3349191d7d5334bce8e85a07
Myles Reid <myles@reidsmailbox.com>
2025-06-16 10:55:57 -0500

    example of Product List set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Context from './components/Context';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div>
       <Context>
+        <ProductList />
       </Context>
     </div>
   )
