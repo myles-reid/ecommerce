a31eb6f2403e6fe8948e2db49240be164beed568
Myles Reid <myles@reidsmailbox.com>
2025-06-16 13:28:25 -0500

    hero set

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,32 @@
+import bannerImage from '../img/banner-img.png';
+
+function HeroBanner() {
+  const circles = []
+
+  for (let i = 0; i < 5; i++) {
+    circles.push(<span className="extra" key={i}></span>);
+  }
+
+  return (
+    <div className="hero-banner">
+      {circles.map(circle => (
+        <span className="extra" key={circle.key}></span>
+      ))}
+      <div className="banner-text">
+        <div className="banner-header">
+          <p>Last Chance</p>
+          <h2>70% <span>off all</span></h2>
+        </div>
+        <div className="banner-subtext">
+          <p>Your favourite looks and style, on sale</p>
+          <p>Enjoy up to 70% off everything in the store</p>
+          <p className="disclaimer">Limited selection available. No refunds or alternatives</p>
+          <button>Shop Now</button>{/*This should ref down to the products section*/}
+        </div>
+      </div>
+      <img className="banner-image" src={bannerImage} alt="Banner Image" />
+    </div>
+  )
+}
+
+export default HeroBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import HeroBanner from "../components/HeroBanner";
+
 function Home() {
   return (
-    <></>
+    <main className="container">
+      <HeroBanner />
+    </main>
   );
 }
 
