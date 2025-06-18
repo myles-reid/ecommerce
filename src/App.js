dc3be18ceec9311994171ad9d6222122ad0ee00a
Myles Reid <myles@reidsmailbox.com>
2025-06-18 09:19:24 -0500

    css fixes for carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import HeaderTop from './components/HeaderTop';
-import Header from './components/Header';
 import Context from './components/Context';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Product from './pages/Product';
 import NotFound from './pages/NotFound';
-import Footer from './components/Footer';
-import Catalogue from './components/Catalogue';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
